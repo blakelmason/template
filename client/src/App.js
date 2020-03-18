7497e22ae1d264c2db7fed898800e0f39c935b2b
Blake Mason <blakelmason@gmail.com>
2020-03-18 14:56:07 -0700

    commit

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     const { user } = this.props
     return (
       <Router>
-        <div>
+        {/* <div>
           <ul>
             <li>
               <Link to="/">Home</Link>
@@ -19,28 +19,19 @@ class App extends Component {
               <Link to="/dashboard">Dashboard</Link>
             </li>
           </ul>
-
-          <hr />
-
-          {/*
-        A <Switch> looks through all its children <Route>
-        elements and renders the first one whose path
-        matches the current URL. Use a <Switch> any time
-        you have multiple routes, but you want only one
-        of them to render at a time
-      */}
-          <Switch>
-            <Route exact path="/">
-              <Home />
-            </Route>
-            <Route path="/about">
-              <About />
-            </Route>
-            <Route path="/dashboard">
-              <Dashboard />
-            </Route>
-          </Switch>
-        </div>
+          <hr /> */}
+        <Switch>
+          <Route exact path="/">
+            {user.loggedIn ? 'Heyo!!!!' : <Redirect />}
+          </Route>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/dashboard">
+            <Dashboard />
+          </Route>
+        </Switch>
+        {/* </div> */}
       </Router>
     )
   }
